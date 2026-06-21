51456a0c6240203a2bde049abcb013957f262d13
itojum <tech@itojum.dev>
2026-06-21 13:01:01 +0900

    answer: 01_block_first_step

--- 04_block/01_block_first_step.rb
+++ 04_block/01_block_first_step.rb
@@ -3,6 +3,9 @@
 #    実行例: MyMath.new.two_times { 2 } #=> 4
 
 class MyMath
+  def two_times
+    yield * 2
+  end
 end
 
 # Q2.
@@ -12,12 +15,15 @@ end
 # AcceptBlockクラスは問題側で用意している(テスト中に実装している)ため実装の必要はありません。
 
 MY_LAMBDA = -> { 3 }
-
+AcceptBlock.call(&MY_LAMBDA)
 # Q3.
 # MyBlockクラスにblock_to_procインスタンスメソッドを定義しましょう。block_to_procインスタンスメソッドはブロックを受け取り、
 # そのブロックをProcオブジェクトにしたものを返します
 
 class MyBlock
+  def block_to_proc(&block)
+    block
+  end
 end
 
 # Q4.
@@ -35,4 +41,9 @@ end
 # さらなる制限として、カウンターとして利用する変数はローカル変数を利用してください(これはテストにはないですが頑張ってローカル変数でテストを通るようにしてみてください)
 
 class MyClosure
+  value = 0
+
+  define_method :increment do
+    value += 1
+  end
 end
